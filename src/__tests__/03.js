39a5fa963152b536eea9feabfc219585210acb47
Rochelle Lewis <rlewis37@gmail.com>
2019-03-22 14:46:26 -0600

    complete exercise 04

--- src/__tests__/03.js
+++ src/__tests__/03.js
@@ -24,8 +24,8 @@ test('renders a toggle component', () => {
 /*
 http://ws.kcd.im/?ws=react%20patterns&e=03&em=rochelle.a.lewis%40gmail.com
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(submitted).toBe(true)
 })
 ////////////////////////////////
--- src/exercises/04.js
+++ src/exercises/04.js
@@ -14,6 +14,7 @@ class Toggle extends React.Component {
         this.props.onToggle(this.state.on)
       },
     )
+
   render() {
     const {on} = this.state
     // We want to give rendering flexibility, so we'll be making
@@ -21,7 +22,10 @@ class Toggle extends React.Component {
     // You'll notice the children prop in the Usage component
     // is a function. 🐨 So you can replace this with a call this.props.children()
     // But you'll need to pass it an object with `on` and `toggle`.
-    return <Switch on={on} onClick={this.toggle} />
+    return (
+      this.props.children({on: this.state.on, toggle: this.toggle})
+    )
+    //return <Switch on={on} onClick={this.toggle} />
   }
 }
 
